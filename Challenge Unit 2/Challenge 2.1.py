e1e8f258cd63ec58b8b6c565fe385aee3d7703bb
msu13220221321506212 <144216394+msu13220221321506212@users.noreply.github.com>
2023-09-16 15:01:43 +0300

    Add files via upload

---
+++ Challenge Unit 2/Challenge 2.1.py
@@ -0,0 +1,30 @@
+class BankAccount:
+  def __init__(self,acno,acname,acbal=0.0):
+    self.__acno = acno
+    self.__acname=acname
+    self.__acbal=acbal
+
+  def deposit(self,amt):
+    if amt > 0:
+      self.__acbal +=amt
+      print("Deposited Rs.{}. Your Current Balance is : Rs.{}".format(amt,self.__acbal))
+    else:
+      print("Invalid Data")
+
+  def withdraw(self,amt):
+    if amt > 0 and amt <= self.__acbal:
+      self.__acbal -= amt
+      print("Withdrew Rs.{}. YourCurrent Balance is Rs.{}".format(amt,self.__acbal))
+    else:
+      print("Invalid Transactions")
+
+  def display(self):
+    a=float(self.__acbal)
+    b=int(self.__acno)
+    print("Hii {} Balance for your Account No. {} is Rs.{}".format(self.__acname,b,a))
+
+
+acc = BankAccount(2004,"Farhaana",5000)
+acc.display()
+acc.deposit(300)
+acc.withdraw(1500)
